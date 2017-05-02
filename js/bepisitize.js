26980f5f64da28268108ede663d826510b7e4a87
James Coombs <jcoombs@jcoombs-p-md01.putnaminv.com>
2017-05-02 15:30:30 -0400

    Toss out bootstrap garbo. Add doggo. Make better var names. Work on img bepisitizment.

--- js/bepisitize.js
+++ js/bepisitize.js
@@ -1,25 +1,29 @@
-var elements = document.getElementsByTagName('*');
+var allElements = document.getElementsByTagName('*');
 
-for (var i = 0; i < elements.length; i++) {
-    var element = elements[i];
+var imgElements = document.getElementsByTagName("img");
 
-    for (var j = 0; j < element.childNodes.length; j++) {
-        var node = element.childNodes[j];
+for (var i = 0; i < allElements.length; i++) {
+    var textElement = allElements[i];
+
+    for (var j = 0; j < textElement.childNodes.length; j++) {
+        var node = textElement.childNodes[j];
 
         if (node.nodeType === 3) {
             var text = node.nodeValue;
             var replacedText = text.replace(/pepsi/gi, 'bepis');
 
             if (replacedText !== text) {
-                element.replaceChild(document.createTextNode(replacedText), node);
+                textElement.replaceChild(document.createTextNode(replacedText), node);
             }
         }
     }
 }
 
-// var imgElements = document.getElementsByTagName("img");
-
 // for (var i = 0; i < imgElements.length; i++) {
-// 	var imgs = imgElements[i];
-// 	imgs[i].src = "";
+// 	var img = imgElements[i];
+// 	console.log('old src ' + img.src);
+// 	if (img.src != " ") {
+// 		img.src = 'ABSOLUTE PATH';
+// 	}
+// 	console.log('new src ' + img.src);
 // }
